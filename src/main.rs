fc7c7b2d7dd235cc11bdbfa4c0e60022ec7450fb
Alexander Kosnac <alexander.kosnac@web.de>
2024-05-05 22:58:31 +0200

    Use resvg to render svg as a png

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+
+fn store_svg_as_png(filepath: String, svg_data: &String, width: u32, height: u32) {
+    let opt = usvg::Options::default();
+    let db = fontdb::Database::new();
+
+    let tree = usvg::Tree::from_str(&svg_data, &opt, &db).unwrap();
+    let transformation = tiny_skia_path::Transform::identity();
+    let mut pixmap = tiny_skia::Pixmap::new(width, height).unwrap();
+    resvg::render(&tree, transformation, &mut pixmap.as_mut());
+    pixmap.save_png(filepath).unwrap();
 }
