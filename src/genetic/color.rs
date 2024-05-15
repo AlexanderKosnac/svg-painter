6e8b80512ecd37c0525ec60b1163b459a28bd551
Alexander Kosnac <alexander.kosnac@web.de>
2024-05-15 21:01:20 +0200

    Also mutate oppacity

--- src/genetic/color.rs
+++ src/genetic/color.rs
@@ -19,19 +19,20 @@ impl Rgba {
 
     pub fn mutate(&mut self, magnitude: f64) {
         let mut rng = rand::thread_rng();
-        let dir_vec: (f64, f64, f64) = (
+        let dir_vec: (f64, f64, f64, f64) = (
             rng.gen_range(-1.0..=1.0),
             rng.gen_range(-1.0..=1.0),
-            rng.gen_range(-1.0..=1.0)
+            rng.gen_range(-1.0..=1.0),
+            rng.gen_range(-1.0..=1.0),
         );
-        let len = (dir_vec.0.powf(2.0) + dir_vec.1.powf(2.0) + dir_vec.2.powf(2.0)).sqrt();
-        let normed = (dir_vec.0/len, dir_vec.1/len, dir_vec.2/len);
-        let scaled = (normed.0 * magnitude, normed.1 * magnitude, normed.2 * magnitude);
+        let len = (dir_vec.0.powf(2.0) + dir_vec.1.powf(2.0) + dir_vec.2.powf(2.0) + dir_vec.2.powf(2.0)).sqrt();
+        let normed = (dir_vec.0/len, dir_vec.1/len, dir_vec.2/len, dir_vec.3/len);
+        let scaled = (normed.0 * magnitude, normed.1 * magnitude, normed.2 * magnitude, normed.3 * magnitude);
 
         self.r = util::bounded_add(self.r, scaled.0 as i64);
         self.g = util::bounded_add(self.g, scaled.1 as i64);
         self.b = util::bounded_add(self.b, scaled.2 as i64);
-        self.a += 0;
+        self.a = util::bounded_add(self.a, scaled.3 as i64);
     }
 }
 
