7c6ece9cb03d1b937f45894d48f095ec166a0420
Alexander Kosnac <alexander.kosnac@web.de>
2024-05-17 22:36:21 +0200

    Rewrite similarity processing and evaluation
    Processing is now done fully in memory, without writing the files to
    disk first.
    Additionally, similarity is now measured based on dot product, not
    euclidean distance.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
 
-use image::{GenericImageView, Rgba};
+use image::{GenericImageView};
+use tiny_skia::{PremultipliedColorU8, ColorU8};
 
 use resvg;
 use usvg;
@@ -31,24 +33,21 @@ fn main() {
 fn evolve<T: Genome + Clone>(raster_image_path: &String, n_generations: u64, genome_size: u32, population_size: u64) {
     let target = read_image(raster_image_path);
 
-    let mut population: Vec<(T, f64)> = (0..population_size).map(|_| (T::new(genome_size, target.width, target.height), 0.0)).collect();
+    let mut population: Vec<(T, u64)> = (0..population_size).map(|_| (T::new(genome_size, target.width(), target.height()), 0)).collect();
 
     let mut generation: u64 = 0;
     loop {
         generation += 1;
-        println!("Generation {}", generation);
+        println!("Generation {}; {} individuals", generation, population.len());
         let dir = format!("build/generation_{:0>3}", generation);
         fs::create_dir_all(&dir).expect("Unable to create generation directory");
 
         let mut i = 0;
         for individual in &mut population {
-            let base = format!("{}/genome_expression_{:0>4}", dir, i);
             let svg_data = individual.0.express();
-            let mut f = File::create(format!("{base}.svg")).expect("Unable to create file");
-            f.write_all(svg_data.as_bytes()).expect("Unable to write data");
-            store_svg_as_png(format!("{base}.png"), &svg_data, target.width, target.height);
-            let candidate_image = read_image(&format!("{base}.png"));
-            individual.1 = candidate_image.distance(&target);
+            let mut candidate = tiny_skia::Pixmap::new(target.width(), target.height()).unwrap();
+            render_svg_into_pixmap(&svg_data, &mut candidate);
+            individual.1 = pixmap_distance(&candidate, &target);
             i += 1;
         }
 
@@ -67,82 +66,50 @@ fn evolve<T: Genome + Clone>(raster_image_path: &String, n_generations: u64, gen
     }
 }
 
-fn setup_population<T: Genome + Clone>(base_individuals: &[(T, f64)], population_size: u64) -> Vec<(T, f64)> {
-    let mut population: Vec<(T, f64)> = Vec::new();
+fn setup_population<T: Genome + Clone>(base_individuals: &[(T, u64)], population_size: u64) -> Vec<(T, u64)> {
+    let mut population: Vec<(T, u64)> = Vec::new();
 
     let individuals_per_genome = population_size / (base_individuals.len() as u64);
     for individual in base_individuals {
         for _ in 0..individuals_per_genome {
             let mut new_genome = individual.0.clone();
             new_genome.mutate();
-            population.push((new_genome, 0.0));
+            population.push((new_genome, 0));
         }
     }
 
     return population;
 }
 
-struct Image {
-    width: u32,
-    height: u32,
-    pixels: Vec<Vec<Rgba<u8>>>,
-}
-
-impl Image {
-    fn distance(&self, other: &Image) -> f64 {
-        if self.width != other.width || self.height != other.height {
-            panic!("Images of different dimensions can not be compared. Got {}x{} and {}x{}.", self.width, self.height, other.width, other.height);
-        }
-        let mut norm: f64 = 0.0;
-        for i in 0..(self.width as usize) {
-            for j in 0..(self.height as usize) {
-                let mut vec_norm: u64 = 0;
-                for k in 0..3 {
-                    let v = abs_diff(self.pixels[j][i].0[k], other.pixels[j][i].0[k]);
-                    vec_norm += (v as u64 * v as u64).pow(2);
-                }
-                norm += (vec_norm as f64).sqrt();
-            }
-        }
-        return (norm as f64).sqrt() as f64;
+fn pixmap_distance(d1: &tiny_skia::Pixmap, d2: &tiny_skia::Pixmap) -> u64 {
+    if d1.width() != d2.width() || d1.height() != d2.height() {
+        panic!("Pixmaps of different dimensions can not be compared. Got {}x{} and {}x{}.", d1.width(), d1.height(), d2.width(), d2.height());
     }
+    let pixels1 = d1.pixels();
+    let pixels2 = d2.pixels();
+    return (0..pixels1.len()).map(|i| coloru8_dot_product(&pixels1[i], &pixels2[i]) as u64).sum::<u64>();
 }
 
-fn abs_diff(a: u8, b: u8) -> u8 {
-    if a > b {
-        return a - b;
-    } else {
-        return b - a;
-    }
+fn coloru8_dot_product(c1: &tiny_skia::PremultipliedColorU8, c2: &tiny_skia::PremultipliedColorU8) -> u32 {
+    return
+        (c1.red() as u32 * c2.red() as u32) +
+        (c1.green() as u32 * c2.green() as u32) +
+        (c1.blue() as u32 * c2.blue() as u32) +
+        (c1.alpha() as u32 * c2.alpha() as u32);
 }
 
-fn read_image(path: &String) -> Image {
-    if let Ok(img) = image::open(path) {
-        let (width, height) = img.dimensions();
-        let mut pixels: Vec<Vec<Rgba<u8>>> = vec![vec![Rgba([0, 0, 0, 0]); width as usize]; height as usize];
-        for y in 0..height {
-            for x in 0..width {
-                let pixel_color = img.get_pixel(x, y);
-                pixels[y as usize][x as usize] = pixel_color;
-            }
-        }
-        return Image {
-            width: width,
-            height: height,
-            pixels: pixels,
-        };
-    } else {
-        panic!("Failed to open the image '{}'.", path);
-    }
+fn read_image(path: &String) -> tiny_skia::Pixmap {
+    println!("-- {}", path);
+    let path_obj = Path::new(path);
+    println!("++ {}", path_obj.display());
+    return tiny_skia::Pixmap::load_png(&path_obj).expect("Failed to open image");
 }
 
-fn store_svg_as_png(filepath: String, svg_data: &String, width: u32, height: u32) {
+fn render_svg_into_pixmap(svg_data: &String, pixmap: &mut tiny_skia::Pixmap) {
     let opt = usvg::Options::default();
     let db = fontdb::Database::new();
 
     let tree = usvg::Tree::from_str(&svg_data, &opt, &db).unwrap();
     let transformation = tiny_skia_path::Transform::identity();
-    let mut pixmap = tiny_skia::Pixmap::new(width, height).unwrap();
     resvg::render(&tree, transformation, &mut pixmap.as_mut());
-    pixmap.save_png(filepath).unwrap();
 }
