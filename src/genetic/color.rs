881bc592e8cae9ff4def151f676509967e878594
Alexander Kosnac <alexander.kosnac@web.de>
2024-06-06 20:35:54 +0200

    Condense color mutation code

--- src/genetic/color.rs
+++ src/genetic/color.rs
@@ -24,20 +24,15 @@ impl Rgba {
 
     pub fn mutate(&mut self, magnitude: f64) {
         let mut rng = rand::thread_rng();
-        let dir_vec: (f64, f64, f64, f64) = (
-            rng.gen_range(-1.0..=1.0),
-            rng.gen_range(-1.0..=1.0),
-            rng.gen_range(-1.0..=1.0),
-            rng.gen_range(-1.0..=1.0),
-        );
-        let len = (dir_vec.0.powf(2.0) + dir_vec.1.powf(2.0) + dir_vec.2.powf(2.0) + dir_vec.3.powf(2.0)).sqrt();
-        let normed = (dir_vec.0/len, dir_vec.1/len, dir_vec.2/len, dir_vec.3/len);
-        let scaled = (normed.0 * magnitude, normed.1 * magnitude, normed.2 * magnitude, normed.3 * magnitude);
+        let dir_vec: Vec<f64> = (0..4).map(|_| rng.gen_range(-1.0..=1.0)).collect();
+        let len = dir_vec.iter().map(|i| i.powf(2.0)).sum::<f64>().sqrt();
+        let normed = dir_vec.iter().map(|i| i/len).collect::<Vec<f64>>();
+        let scaled = normed.iter().map(|i| i*magnitude).collect::<Vec<f64>>();
 
-        self.r = util::bounded_add(self.r, scaled.0 as i64);
-        self.g = util::bounded_add(self.g, scaled.1 as i64);
-        self.b = util::bounded_add(self.b, scaled.2 as i64);
-        self.a = util::bounded_add(self.a, scaled.3 as i64);
+        self.r = util::bounded_add(self.r, scaled[0] as i64);
+        self.g = util::bounded_add(self.g, scaled[1] as i64);
+        self.b = util::bounded_add(self.b, scaled[2] as i64);
+        self.a = util::bounded_add(self.a, scaled[3] as i64);
     }
 }
 
