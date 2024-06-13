077b5934d3f732a570ff697e06ff7bcebdd53d83
Alexander Kosnac <alexander.kosnac@web.de>
2024-06-13 23:09:31 +0200

    Separate genome and base into separate files

--- src/genetic.rs
+++ src/genetic.rs
@@ -16,10 +16,14 @@ use crate::BUILD;
 use std::cmp;
 use rand::Rng;
 
-use crate::genetic::color::Rgba;
 use crate::Controller;
+use crate::genetic::color::Rgba;
+use crate::genetic::base::StrokeBase;
+use crate::genetic::genome::SvgElementGenome;
 
 pub mod color;
+pub mod base;
+pub mod genome;
 
 pub struct Experiment {
     controller_rc: Rc<RefCell<Controller>>,
@@ -143,158 +147,5 @@ impl Experiment {
             new_population.push((child, 0.0));
         }
         self.population = new_population;
-    }    
-}
-
-static STROKES: [&str; 3] = [
-    "<rect id=\"stroke-0\" width=\"100\" height=\"25\"/>",
-    "<rect id=\"stroke-1\" width=\"100\" height=\"50\"/>",
-    "<rect id=\"stroke-2\" width=\"100\" height=\"75\"/>",
-];
-
-pub struct StrokeBase {
-    controller_rc: Rc<RefCell<Controller>>,
-    stroke_idx: usize,
-    x: i32,
-    y: i32,
-    rotation: i32,
-    scale_x: f32,
-    scale_y: f32,
-    color: Rgba,
-}
-
-impl StrokeBase {
-    pub fn new(controller_rc: Rc<RefCell<Controller>>) -> Self {
-        let mut rng = rand::thread_rng();
-        let (x, y) = controller_rc.borrow().get_xy();
-        let (scale_x, scale_y) = controller_rc.borrow().get_scale();
-        Self {
-            controller_rc: controller_rc,
-            stroke_idx: rng.gen_range(0..STROKES.len()) as usize,
-            x: x,
-            y: y,
-            rotation: rng.gen_range(0..360),
-            scale_x: scale_x,
-            scale_y: scale_y,
-            color: Rgba::new_rand(),
-        }
-    }
-
-    fn express(&self) -> String {
-        let stroke = format!("<use href=\"#stroke-{}\"/>", self.stroke_idx);
-        let transformations = format!("translate({} {}) rotate({}) scale({:.5} {:.5})", self.x, self.y, self.rotation, self.scale_x, self.scale_y);
-        return format!("<g fill-opacity=\"{:.3}\" fill=\"{}\" transform=\"{}\">{}</g>", (self.color.a as f64)/255.0, self.color.as_hex(), transformations, stroke);
-    }
-
-    fn mutate(&mut self) {
-        let mut rng = rand::thread_rng();
-
-        match rng.gen_range(0..=2) {
-            0 => (self.x, self.y) = self.controller_rc.borrow().get_xy(),
-            1 => self.rotation = rng.gen_range(0..360),
-            2 => self.color = Rgba::new_rand(),
-            _ => panic!("Cannot happen."),
-        }
     }
 }
-
-impl Clone for StrokeBase {
-    fn clone(&self) -> Self {
-        Self {
-            controller_rc: Rc::clone(&self.controller_rc),
-            stroke_idx: self.stroke_idx,
-            x: self.x,
-            y: self.y,
-            rotation: self.rotation,
-            scale_x: self.scale_x,
-            scale_y: self.scale_y,
-            color: self.color.clone(),
-        }
-    }
-}
-
-pub struct SvgElementGenome {
-    controller_rc: Rc<RefCell<Controller>>,
-    sequence: Vec<StrokeBase>,
-    sequence_fixed: Vec<StrokeBase>,
-    width: u32,
-    height: u32,
-}
-
-impl SvgElementGenome {
-    fn new(controller_rc: Rc<RefCell<Controller>>, width: u32, height: u32) -> Self {
-        Self {
-            controller_rc: controller_rc,
-            sequence: Vec::new(),
-            sequence_fixed: Vec::new(),
-            width: width,
-            height: height,
-        }
-    }
-
-    fn express(&self) -> String {
-        let expressed: String = self.sequence_fixed.iter().chain(self.sequence.iter()).map(|b| b.express()).collect::<Vec<String>>().join("\n");
-        return format!(
-            "<svg width=\"{}\" height=\"{}\" xmlns=\"http://www.w3.org/2000/svg\">\n<def>\n{}\n</def>\n<rect width=\"100%\" height=\"100%\" fill=\"white\"/>\n{expressed}\n</svg>",
-            self.width, self.height, STROKES.join("\n")
-        );
-    }
-
-    fn mutate(&mut self) {
-        let mut rng = rand::thread_rng();
-
-        let range_max = self.sequence.len();
-        let bases_to_mutate = cmp::max(1, (range_max as f64 * 0.05) as u64);
-        let candidates: Vec<usize> = (0..bases_to_mutate).map(|_| rng.gen_range(0..range_max)).collect();
-
-        for c in candidates {
-            self.sequence[c].mutate();
-        }
-    }
-
-    fn cross_with(&self, other: &SvgElementGenome) -> SvgElementGenome {
-        let mut rng = rand::thread_rng();
-
-        let mut sequences = vec![&self.sequence, &other.sequence];
-        sequences.sort_by(|a, b| a.len().cmp(&b.len()));
-        let (short, long) = (sequences[0], sequences[1]);
-
-        let crossover_point_idx = rng.gen_range(0..long.len());
-
-        let mut new_sequence = Vec::new();
-        new_sequence.extend_from_slice(&short[0..crossover_point_idx]);
-        new_sequence.extend_from_slice(&long[crossover_point_idx..]);
-
-        SvgElementGenome {
-            controller_rc: Rc::clone(&self.controller_rc),
-            sequence: new_sequence,
-            sequence_fixed: self.sequence_fixed.clone(),
-            width: self.width,
-            height: self.height,
-        }
-    }
-
-    fn insertion(&mut self, n: u64) {
-        for _ in 0..n {
-            self.sequence.push(StrokeBase::new(Rc::clone(&self.controller_rc)));
-        }
-    }
-
-    fn fixate(&mut self) {
-        self.sequence.append(&mut self.sequence_fixed);
-    }
-}
-
-impl Clone for SvgElementGenome {
-    fn clone(&self) -> Self {
-        Self {
-            controller_rc: Rc::clone(&self.controller_rc),
-            sequence: self.sequence.clone(),
-            sequence_fixed: self.sequence_fixed.clone(),
-            width: self.width,
-            height: self.height,
-        }
-    }
-}
-
-unsafe impl Send for SvgElementGenome {}
---
+++ src/genetic/base.rs
@@ -0,0 +1,89 @@
+use tiny_skia;
+
+use rayon::prelude::*;
+
+use std::fs::File;
+use std::io::Write;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use rand_distr::Distribution;
+
+use crate::util;
+
+use crate::BUILD;
+
+use std::cmp;
+use rand::Rng;
+
+use crate::Controller;
+use crate::genetic::color::Rgba;
+use crate::genetic::genome::SvgElementGenome;
+
+pub static STROKES: [&str; 4] = [
+    "<rect id=\"stroke-0\" width=\"100\" height=\"25\"/>",
+    "<rect id=\"stroke-1\" width=\"100\" height=\"50\"/>",
+    "<rect id=\"stroke-2\" width=\"100\" height=\"75\"/>",
+    "<rect id=\"stroke-3\" width=\"100\" height=\"100\"/>",
+];
+
+pub struct StrokeBase {
+    controller_rc: Rc<RefCell<Controller>>,
+    stroke_idx: usize,
+    x: i32,
+    y: i32,
+    rotation: i32,
+    scale_x: f32,
+    scale_y: f32,
+    color: Rgba,
+}
+
+impl StrokeBase {
+    pub fn new(controller_rc: Rc<RefCell<Controller>>) -> Self {
+        let mut rng = rand::thread_rng();
+        let (x, y) = controller_rc.borrow().get_xy();
+        let (scale_x, scale_y) = controller_rc.borrow().get_scale();
+        Self {
+            controller_rc: controller_rc,
+            stroke_idx: rng.gen_range(0..STROKES.len()) as usize,
+            x: x,
+            y: y,
+            rotation: rng.gen_range(0..360),
+            scale_x: scale_x,
+            scale_y: scale_y,
+            color: Rgba::new_rand(),
+        }
+    }
+
+    pub fn express(&self) -> String {
+        let stroke = format!("<use href=\"#stroke-{}\"/>", self.stroke_idx);
+        let transformations = format!("translate({} {}) rotate({}) scale({:.5} {:.5})", self.x, self.y, self.rotation, self.scale_x, self.scale_y);
+        return format!("<g fill-opacity=\"{:.3}\" fill=\"{}\" transform=\"{}\">{}</g>", (self.color.a as f64)/255.0, self.color.as_hex(), transformations, stroke);
+    }
+
+    pub fn mutate(&mut self) {
+        let mut rng = rand::thread_rng();
+
+        match rng.gen_range(0..=2) {
+            0 => (self.x, self.y) = self.controller_rc.borrow().get_xy(),
+            1 => self.rotation = rng.gen_range(0..360),
+            2 => self.color = Rgba::new_rand(),
+            _ => panic!("Cannot happen."),
+        }
+    }
+}
+
+impl Clone for StrokeBase {
+    fn clone(&self) -> Self {
+        Self {
+            controller_rc: Rc::clone(&self.controller_rc),
+            stroke_idx: self.stroke_idx,
+            x: self.x,
+            y: self.y,
+            rotation: self.rotation,
+            scale_x: self.scale_x,
+            scale_y: self.scale_y,
+            color: self.color.clone(),
+        }
+    }
+}
